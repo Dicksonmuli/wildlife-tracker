2953a686dacb1d589d0cf47e593dc1b809187579
dicksonmuli <dicksonmully6@gmail.com>
2017-05-10 12:09:05 +0300

    connecting to database-heroku

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,6 +1,6 @@
 import org.sql2o.*;
 
 public class DB {
-  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "dickson", "dickson");
-//   public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-50-17-236-15.compute-1.amazonaws.com:5432/d7jf4e2jrk48mq", "phtrzuiktlilam", "ed5c7178459027617f8294f3380dc9fae0341f5dbc43f3fab70915571edc32e7");
+  // public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "dickson", "dickson");
+  public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-50-17-236-15.compute-1.amazonaws.com:5432/d7jf4e2jrk48mq", "phtrzuiktlilam", "ed5c7178459027617f8294f3380dc9fae0341f5dbc43f3fab70915571edc32e7");
 }
