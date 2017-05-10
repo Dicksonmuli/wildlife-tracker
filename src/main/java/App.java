f520183fd53d1714a19313db06fb14b7d3e5f258
dicksonmuli <dicksonmully6@gmail.com>
2017-05-10 11:23:39 +0300

    main method class with a port detection method

---
+++ src/main/java/App.java
@@ -0,0 +1,33 @@
+import org.sql2o.*;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+	public static void main(String[] args) {
+		staticFileLocation("/public");
+		String layout = "templates/layout.vtl";
+		// if a port is set for the app, use it else continue with 4567
+		ProcessBuilder process = new ProcessBuilder();
+		Integer port;
+		if (process.environment().get("PORT") !=null) {
+			port = Integer.parseInt(process.environment().get("PORT"));
+		}else {
+			port = 4567;
+		}
+		setPort(port);
+
+		// root route
+		// get("/", (request, response) -> {
+		// 	Map<String, Object> model = new HashMap<String, Object>();
+		// 	// model.put("tasks", request.session().attribute("tasks"));
+		// 	model.put("categories", Category.all());
+		// 	model.put("template", "templates/index.vtl");
+		// 	return new ModelAndView(model, layout);
+		// }, new VelocityTemplateEngine());
+
+	}
+}
