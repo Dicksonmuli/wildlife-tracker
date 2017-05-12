339d728cb3d67c12fb7c78dfc3b2b5ec34060c35
dicksonmuli <dicksonmully6@gmail.com>
2017-05-12 15:28:28 +0300

    sightings form and endangered form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -41,15 +41,50 @@ public class App {
 			model.put("template", "templates/sighting-form.vtl");
 			return new ModelAndView(model, layout);
 		}, new VelocityTemplateEngine());
-		//categories
+		//sightings
 		post("/sightings", (request, response) -> {
 			Map<String, Object> model = new HashMap<String, Object>();
-			String name = request.queryParams("name");
-			Category newCategory = new Category(name);
-			newCategory.save();
+			String rangerName = request.queryParams("rangerName");
+			String location = request.queryParams("location");
+			int age = Integer.parseInt(request.queryParams("age"));
+			String type = request.queryParams("health");
+			String name =request.queryParams("animalName");
+
+			Animal newAnimal = new Animal(name);
+			try{
+				Sighting sighting = new Sighting(animalId,location, rangerName);
+				newAnimal.save();
+			}catch (IllegalArgumentException exception) {
+				System.out.println("Animal Name, Location and Ranger Name Required")
+			}
 			model.put("template", "templates/sightings.vtl");
  			return new ModelAndView(model, layout);
 			}, new VelocityTemplateEngine());
+
+			//endangered
+			get("/endangered/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/endangered-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+			//endangered post
+			post("/endangered/new", (request, response) -> {
+				Map<String, Object> model = new HashMap<String, Object>();
+				int age = Integer.parseInt(request.queryParams("age"));
+				String health = request.queryParams("health");
+				String name =request.queryParams("animalName");
+				Animal newAnimal = new Animal(name);
+				try{
+					Endangered newEndangered = new Endangered(name, health, age);
+					newAnimal.save();
+				}catch (IllegalArgumentException exception) {
+					System.out.println("Animal Name, health and age Required")
+				}
+				model.put("template", "templates/endangered-form.vtl");
+	 			return new ModelAndView(model, layout);
+				}, new VelocityTemplateEngine());
+			//
 
 	}
 }
